7f0145844f3c64fdd39c02b32adccd21deae42e9
shotarokubo <kuboooon48@gmail.com>
2026-03-02 01:44:57 +0900

    Update marche_monitor.py

--- marche_monitor.py
+++ marche_monitor.py
@@ -15,7 +15,7 @@ TARGET_CREATORS = [
     {"name": "柏葉れん", "id": "dst_kashiwabare", "nickname": "れん|れんちゃん|かし|カシ|ドム|ジオング|かしわば"},
     {"name": "瀬﨑くるみ", "id": "dst_sezakikurum", "nickname": "陶芸家|くるみん|せざき|せざくる|セザクル"},
     {"name": "詩之宮かこ", "id": "chum_shinomiyak", "nickname": "かこちゃん|かこちま|ちま|かこち|しのみや"},
-    {"name": "ChumToto", "id": "chumtoto", "nickname": "ちゅむとと|公式"},
+    {"name": "ChumToto", "id": "chumtoto", "nickname": "ちゃむとと|公式|ちゃむ|チャム|チャムトト"},
 ]
 
 DB_FILE = "chum_last_inventory.json"
